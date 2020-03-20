04199c94a9d9db27dc0da8435365f8c93b424cab
Liz Song <lizs20@lakesideschool.org>
2020-03-19 18:12:50 -0700

    added the humanoid figure and enabled collisions with it

---
+++ Map1/Assets/CollisionScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    private void OnControllerColliderHit (ControllerColliderHit hit)
+        {
+        	if (hit.gameObject.tag == "oldman")
+     		{
+         		Debug.Log("You have hit Harold");
+     		}
+        }
+}
