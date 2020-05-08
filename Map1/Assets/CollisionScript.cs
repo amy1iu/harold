32ef1b1879300ea5dea0f3585b4c5bc87479b4b5
Liz Song <lizs20@lakesideschool.org>
2020-05-07 22:26:31 -0700

    added background music and audio triggers for certain events (hitting food, getting tagged by librarian)

--- Map1/Assets/CollisionScript.cs
+++ Map1/Assets/CollisionScript.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class CollisionScript : MonoBehaviour
 {
+    
+	public AudioSource backgroundmusic;
+	public AudioSource foodSFX;
+	public AudioSource gameover;
+
+	void Start()
+    {
+        backgroundmusic.gameObject.GetComponent<AudioSource>().Play();
+    }
+
     private void OnControllerColliderHit (ControllerColliderHit hit)
         {
         	if (hit.gameObject.tag == "oldman")
      		{
          		this.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+         		backgroundmusic.gameObject.GetComponent<AudioSource>().Stop();
+         		gameover.gameObject.GetComponent<AudioSource>().Play();
+     		}
+
+     		if (hit.gameObject.tag == "food")
+     		{
+     			foodSFX.gameObject.GetComponent<AudioSource>().Play();
      		}
         }
 }
