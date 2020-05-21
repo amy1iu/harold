1e06f25b82709e31eae8edf330cfabf44980834d
Liz Song <lizs20@lakesideschool.org>
2020-05-21 00:23:09 -0700

    added cutscene at the start of the map scene

---
+++ Map1/Assets/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+	public GameObject playerCam;
+    public GameObject cutsceneCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	cutsceneCam.SetActive(true);
+        playerCam.SetActive(false);
+        StartCoroutine(FinishCut());
+    }
+
+    IEnumerator FinishCut()
+    {
+        yield return new WaitForSeconds(6);
+        cutsceneCam.SetActive(false);
+        playerCam.SetActive(true);
+    }
+}
